87e9de0aea51bd502c72229bf96d3c9c92964664
Roman Gorokhovsky <76645646+madmongoose@users.noreply.github.com>
2021-07-12 15:53:03 +0300

    Update wp-config.php

--- terraform/wp-config.php
+++ terraform/wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'admin' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'test' );
+define( 'DB_PASSWORD', 'test1234' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
