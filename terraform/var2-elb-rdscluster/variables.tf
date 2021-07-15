d818caf4d2893b57a46c23424a97b52104545fc9
Roman Gorokhovsky <76645646+madmongoose@users.noreply.github.com>
2021-07-15 16:21:29 +0300

    Update variables.tf

--- terraform/var2-elb-rdscluster/variables.tf
+++ terraform/var2-elb-rdscluster/variables.tf
@@ -1,5 +1,5 @@
-//variable "aws_access_key" {}
-//variable "aws_secret_key" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 variable "generated_key_name" {
   type        = string
   //default     = "terraform-key-pair"
