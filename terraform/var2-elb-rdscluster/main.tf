252745827247af7c676294c5e702014645c94e17
Roman Gorokhovsky <76645646+madmongoose@users.noreply.github.com>
2021-07-15 16:22:24 +0300

    Update main.tf

--- terraform/var2-elb-rdscluster/main.tf
+++ terraform/var2-elb-rdscluster/main.tf
@@ -13,8 +13,8 @@
 
 provider "aws" {
     region = "eu-west-2"
-    //access_key = var.aws_access_key
-    //secret_key = var.aws_secret_key
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
 }
 
 data "aws_availability_zones" "available" {}
